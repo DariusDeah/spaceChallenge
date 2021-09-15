00559ccbb46dd2a5b8bcd26142de93ae60585421
DariusDeah <dariusdeah21@gmail.com>
2021-09-15 14:39:43 -0600

    building get method

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
+import { GalaxySchema } from '../models/Galaxy.js'
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
-  Galaxy = mongoose.model('Galaxy')
+  Galaxy = mongoose.model('Galaxy', GalaxySchema)
 }
 
 export const dbContext = new DbContext()
